769d8f3106ea8a4694563e7c6ed8dea1411c24f2
Lucas Dias <lucasdiasupf@gmail.com>
2022-10-03 21:17:37 -0300

    comit ex

--- config.js
+++ config.js
@@ -9,9 +9,11 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL: connectionString
   // descomentar para fazer deploy no heroku
-  , ssl: {
-    rejectUnauthorized: false,
-  }
+  //, ssl: {
+  //  rejectUnauthorized: false,
+ // }
 })
 
 module.exports = {pool}
+
+//test deploy 21:16 03-10 gaga
